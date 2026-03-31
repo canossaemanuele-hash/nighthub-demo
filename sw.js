86346198012479c574b4a6ee407950a537bcf591
canossaemanuele-hash <canossa.emanuele@gmail.com>
2026-03-31 14:26:40 +0200

    Change CACHE_NAME and implement cache cleanup
    Updated cache name and added activation event to handle old caches.

--- sw.js
+++ sw.js
@@ -1,13 +1,25 @@
-const CACHE_NAME = 'tablekey-v1';
+const CACHE_NAME = 'malua-v1';
 const urlsToCache = [
   './index.html',
   './manifest.json'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                  .map(name => caches.delete(name))
+      );
+    })
   );
 });
 
